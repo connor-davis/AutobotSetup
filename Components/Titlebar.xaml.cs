2160eef05f437d60dfc169a6a58baa7663fb6182
Connor Davis <cnnrproton@gmail.com>
2023-10-10 04:46:31 +0200

    UI Updates:
    - Added titlebar.
    - Added welcome message and continue button

---
+++ Components/Titlebar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AutobotSetup.Components;
+
+public partial class Titlebar : UserControl
+{
+    public Titlebar()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+
+    private void DragArea_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            MainWindow.GetInstance()?.DragMove();
+        }
+    }
+}
